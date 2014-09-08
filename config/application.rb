4760f6d9bc04e1c71c0f704f33b706efe38ec83a
Juan F. Pérez <j.guitar@gmail.com>
2014-09-08 13:24:34 +0200

    setting spanish, copied Rails and Devise locale resources

--- config/application.rb
+++ config/application.rb
@@ -39,6 +39,6 @@ module AdoptAPet
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :es
   end
 end
