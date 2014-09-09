37f8303a04be68b730d30fc0f88c800e2e247bd6
Juan F. Pérez <j.guitar@gmail.com>
2014-09-09 12:39:58 +0200

    form with Rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :pets, only: [:show, :new, :create, :edit, :update, :destroy]
+  resources :pets, only: [:show, :new, :create, :edit, :update, :destroy] do
+    collection do
+      post :find
+    end
+  end
 
   ActiveAdmin.routes(self)
   devise_for :users
