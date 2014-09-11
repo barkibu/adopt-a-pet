f3ca1dc76f98839273609e181f4179c24b0545a9
Juan F. Pérez <j.guitar@gmail.com>
2014-09-11 10:57:10 +0200

    fixing warning

--- app/models/pet_picture.rb
+++ app/models/pet_picture.rb
@@ -1,11 +1,10 @@
 class PetPicture < ActiveRecord::Base
   belongs_to :pet
 
-  has_attached_file :asset, {
+  has_attached_file :asset,
     styles: { home: "340x150#", vertical: "360x640>", thumb: "100x100>" },
     convert_options: { home: "-strip", vertical: "-strip", thumb: "-quality 75 -strip"},
-    default_url: "/images/:style/missing.png",
-  }
+    default_url: "/images/:style/missing.png"
   validates_attachment_content_type :asset, content_type: /\Aimage\/.*\Z/
   validates_attachment_presence :asset
   validates_attachment_size :asset, less_than: 5.megabytes
