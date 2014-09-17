0b619922fa78623a2c7b8781c89d419b795173fc
Edu Saborit <edu@barkibu.com>
2014-09-17 19:02:02 +0200

    Making ApplicationController#current_admin_user accesible as helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  helper_method :current_admin_user
+
   def authenticate_admin_user!
     authenticate_user!
     unless current_user.admin?
