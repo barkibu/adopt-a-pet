b50871601593b979f398c12d6ce311ef36d6b085
Juan F. Pérez <j.guitar@gmail.com>
2014-09-18 11:50:41 +0200

    adding logo

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,3 +1,10 @@
 class Shelter < ActiveRecord::Base
+  has_attached_file :logo,
+    styles: { thumb: "160x50>" },
+    convert_options: { thumb: "-quality 75 -strip"},
+    default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :logo, content_type: /\Aimage\/.*\Z/
+  validates_attachment_size :logo, less_than: 5.megabytes
+
   validates :name, presence: true
 end
