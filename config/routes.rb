a002087182d19452d53544037581dbbdf15a216a
Juan F. Pérez <j.guitar@gmail.com>
2014-09-18 18:37:48 +0200

    translating shelters and routes improvements

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
 
-  resources :shelters
-
-  resources :pets, only: [:show, :new, :create, :edit, :update, :destroy]
-
-  post '/find' => 'home#find', as: :find
+  resources :shelters, path: 'protectoras-de-animales'
+  resources :pets, only: [:new, :create, :edit, :update, :destroy]
 
   ActiveAdmin.routes(self)
   devise_for :users
-  root :to => 'home#index'
 
+  root :to => 'home#index'
+  post '/find' => 'home#find', as: :find
   get '/en-adopcion/:specie(/:location)(/:breed)' => 'home#adopt', as: :adopt_species
   get '/en-adopcion/:specie/:location/:breed/:id' => 'pets#show', as: :adopt_specie
 end
