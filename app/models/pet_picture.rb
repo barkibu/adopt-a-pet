2b762f890d42a6981980f3fb5b6ee1130f5444ec
Edu Saborit <edu@barkibu.com>
2014-09-30 14:10:25 +0200

    Optimizing image formats

--- app/models/pet_picture.rb
+++ app/models/pet_picture.rb
@@ -4,12 +4,12 @@ class PetPicture < ActiveRecord::Base
   has_attached_file :asset,
     styles: {
     home: "",
-    vertical: "360x640>",
+    show_main: "640x360>",
     thumb: "100x100>",
   },
   convert_options: {
     home: "-strip -gravity north -thumbnail 456x342^ -extent 456x342",
-    vertical: "-strip",
+    show_main: "-strip",
     thumb: "-quality 75 -strip",
   },
   default_url: "/images/:style/missing.png"
