6695d8ca78df18f8066a317dcd7d7d73d154dea0
Juan F. Pérez <j.guitar@gmail.com>
2014-10-13 14:16:18 +0200

    adding-elrefugio

--- app/models/tentacles/feeds.rb
+++ app/models/tentacles/feeds.rb
@@ -6,4 +6,6 @@ module Tentacles::Feeds
   CIAAM_CATS = { name: 'ciaam_cats_adopt-a-pet' }
   CIUDAD_ANIMAL_DOGS = { name: 'ciudad_animal_dogs_adopt-a-pet' }
   CIUDAD_ANIMAL_CATS = { name: 'ciudad_animal_cats_adopt-a-pet' }
+  EL_REFUGIO_DOGS = { name: 'el_refugio_dogs_adopt-a-pet' }
+  EL_REFUGIO_CATS = { name: 'el_refugio_cats_adopt-a-pet' }
 end
