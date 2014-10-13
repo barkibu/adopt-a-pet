6e128e0704b8b510dd2a1c2fdb9762c778e2befc
Juan F. Pérez <j.guitar@gmail.com>
2014-10-13 17:03:21 +0200

    simplifying names

--- app/helpers/pets_helper.rb
+++ app/helpers/pets_helper.rb
@@ -1,20 +1,16 @@
 module PetsHelper
-  def breadcrumb_specie_path(params)
-    adopt_species_path(params.slice :specie)
-  end
-
-  def breadcrumb_province_path(params)
+  def adopt_province_path(params)
     adopt_species_path(params.slice :specie, :province)
   end
 
-  def breadcrumb_breed_path(params)
+  def adopt_breed_path(params)
     adopt_species_path(params.slice :specie, :province, :breed)
   end
 
   def link_province_path(specie, province)
     specie_text = Specie.to_s(specie)
     title = "#{specie_text.capitalize} en adopción en #{province.name}"
-    path = breadcrumb_province_path(specie: specie_text, province: province)
+    path = adopt_province_path(specie: specie_text, province: province)
     link_to province.name, path, title: title
   end
 end
