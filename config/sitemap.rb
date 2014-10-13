3daad2d7aa44af2118fe39915d2199ef4cf05ea2
Juan F. Pérez <j.guitar@gmail.com>
2014-10-13 18:36:19 +0200

    avoiding duplicates on sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -4,14 +4,13 @@ require 'sitemap_generator'
 SitemapGenerator::Sitemap.default_host = 'http://www.barkiadopta.org'
 SitemapGenerator::Sitemap.create do
   add '/', changefreq: 'weekly'
-  Province.all.each do |province|
-    add '/en-adopcion/mascotas/', changefreq: 'weekly'
-    add "/en-adopcion/mascotas/#{province.slug}/", changefreq: 'weekly'
-    add '/en-adopcion/perros/', changefreq: 'weekly'
-    add "/en-adopcion/perros/#{province.slug}/", changefreq: 'weekly'
-    add '/en-adopcion/gatos/', changefreq: 'weekly'
-    add "/en-adopcion/gatos/#{province.slug}/", changefreq: 'weekly'
-    add '/protectoras-de-animales', changefreq: 'monthly'
+
+  %w(mascotas perros gatos).each do |pet|
+    add "/en-adopcion/#{pet}/", changefreq: 'weekly'
+    Province.all.each do |province|
+      add "/en-adopcion/#{pet}/#{province.slug}/", changefreq: 'weekly'
+    end
   end
+  add '/protectoras-de-animales', changefreq: 'monthly'
 end
 # SitemapGenerator::Sitemap.ping_search_engines # Not needed if you use the rake tasks
