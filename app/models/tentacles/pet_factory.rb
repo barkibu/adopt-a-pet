9dc23063c7784077d62244cfddff045440bed9b3
Juan F. Pérez <j.guitar@gmail.com>
2014-10-14 17:13:17 +0200

    moving methods

--- app/models/tentacles/pet_factory.rb
+++ app/models/tentacles/pet_factory.rb
@@ -1,19 +1,6 @@
 class Tentacles::PetFactory
   def self.update_pet_and_imported_pet(imported_pet, object)
     pet = pet_from_imported_pet_or_object(imported_pet, object)
-    save_pet_and_imported_pet(pet, imported_pet, object)
-  end
-
-  def self.pet_from_imported_pet_or_object(imported_pet, object)
-    imported_pet.pet || Pet.new(Tentacles::PetAttributesJSONParser.new.parse(object))
-  end
-
-  def self.imported_pet_from_object(object)
-    imported_pet = ImportedPet.find_by(url: object['more_info_url'])
-    imported_pet ||= ImportedPet.find_or_initialize_by(data: object.to_s)
-  end
-
-  def self.save_pet_and_imported_pet(pet, imported_pet, object)
     if pet.new_record?
       pet.created_at = object['created_at']
     end
@@ -28,15 +15,7 @@ class Tentacles::PetFactory
     pet.status = object['status']
     pet.urgent = object['urgent']
 
-    if object['img'].present?
-      picture = pet.pet_pictures.new
-      begin
-        picture.asset = object['img']
-      rescue OpenURI::HTTPError, SocketError => e
-        imported_pet.add_log("Img <#{object['img']}> is not valid. Error: #{e.message}.")
-        picture.destroy
-      end
-    end
+    add_image(pet, imported_pet, object['img'])
 
     if pet.save
       imported_pet.pet_id = pet.id
@@ -44,5 +23,26 @@ class Tentacles::PetFactory
       imported_pet.add_log(pet.errors.messages.to_s)
     end
     imported_pet.save!
+  end
+
+  def self.pet_from_imported_pet_or_object(imported_pet, object)
+    imported_pet.pet || Pet.new(Tentacles::PetAttributesJSONParser.new.parse(object))
+  end
+
+  def self.imported_pet_from_object(object)
+    imported_pet = ImportedPet.find_by(url: object['more_info_url'])
+    imported_pet ||= ImportedPet.find_or_initialize_by(data: object.to_s)
+  end
+
+  def self.add_image(pet, image)
+    if image.present?
+      picture = pet.pet_pictures.new
+      begin
+        picture.asset = image
+      rescue OpenURI::HTTPError, SocketError => e
+        imported_pet.add_log("Img <#{image}> is not valid. Error: #{e.message}.")
+        picture.destroy
+      end
+    end
   end
 end
