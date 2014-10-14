4d6d69546a518445992eb2206388c3f2d00c28c2
Juan F. Pérez <j.guitar@gmail.com>
2014-10-14 12:58:50 +0200

    ImportedPet.url to have a _unique_ field to find them

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141010114720) do
+ActiveRecord::Schema.define(version: 20141014103421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,9 +53,11 @@ ActiveRecord::Schema.define(version: 20141010114720) do
     t.text     "logs"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "url"
   end
 
   add_index "imported_pets", ["pet_id"], name: "index_imported_pets_on_pet_id", using: :btree
+  add_index "imported_pets", ["url"], name: "index_imported_pets_on_url", using: :btree
 
   create_table "pet_pictures", force: true do |t|
     t.integer  "pet_id"
