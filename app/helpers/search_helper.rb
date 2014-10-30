a426a3a948a9b7975a2bf04c19f4c01f9e01fc7f
Juan F. Pérez <j.guitar@gmail.com>
2014-10-30 16:54:22 +0100

    title generated dynamically

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -10,4 +10,13 @@ module SearchHelper
   def check_specie_cat?(params)
     check_specie?(params) || params[:specie] == 'gatos'
   end
+
+  def submit_serch_text(specie)
+    specie_key = Specie.find_by_specie(specie)
+    if Specie.key_without_pet?(specie_key)
+      "Buscar #{specie} en adopción"
+    else
+      'Buscar animales en adopción'
+    end
+  end
 end
