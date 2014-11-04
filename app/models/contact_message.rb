3d36b86795987be640e864b2d5b59aa005cf03aa
Juan F. Pérez <j.guitar@gmail.com>
2014-11-04 13:57:34 +0100

    using Rails to translate classes

--- app/models/contact_message.rb
+++ app/models/contact_message.rb
@@ -1,8 +1,13 @@
 class ContactMessage
   include ActiveModel::Model
+  extend ActiveModel::Translation
 
   attr_accessor :name, :email, :subject, :body
 
   validates :name, :email, :subject, :body, presence: true
   validates :email, email: true, allow_blank: true
+
+  def self.i18n_scope
+    :activerecord
+  end
 end
