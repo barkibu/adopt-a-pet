4d75d22acca87cd5ff20901031c61c0cb484352e
Juan F. Pérez <j.guitar@gmail.com>
2014-11-05 12:20:46 +0100

    Improve name

--- app/models/concerns/filterable.rb
+++ app/models/concerns/filterable.rb
@@ -2,9 +2,9 @@ module Filterable
   extend ActiveSupport::Concern
 
   module ClassMethods
-    def filter(filtering_params)
+    def filter(params)
       results = self.where(nil)
-      filtering_params.each do |key, value|
+      params.each do |key, value|
         results = results.public_send("filter_#{key}", value) if value.present?
       end
       results
