1857a360b6b58a99931c65c7d7c3cf3e5f7905af
Edu Saborit <edu@barkibu.com>
2018-03-19 13:15:57 +0100

    Change references from barkiadopta to kiwokoadopta

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'www.barkiadopta.org' }
+  config.action_mailer.default_url_options = { host: 'www.kiwokoadopta.org' }
 
   config.paperclip_defaults = {
     storage: :s3,
